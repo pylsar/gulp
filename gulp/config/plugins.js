fa17c8cce5ce95345fc9d526f51e83b6c59c48f7
Alex Kusov <kysov2006@bk.ru>
2021-12-25 23:27:43 +0300

    error doesnt work

---
+++ gulp/config/plugins.js
@@ -0,0 +1,10 @@
+import replace from 'gulp-replace'; // поиск и замена
+import plumber from 'gulp-plumber'; // обработка ошибок
+import notify from 'gulp-notify'; // подсказки
+ 
+// экспортируем объекты
+export const plugins = {
+    replace: replace,
+    plumber: plumber,
+    notify: notify
+}
--- gulpFile.js
+++ gulpFile.js
@@ -4,23 +4,31 @@ import gulp from 'gulp';
 // импорт путей
 import {path} from './gulp/config/path.js';
 
+//импорт плагинов
+import {plugins} from './gulp/config/plugins.js';
+
 // передаем значения в глобальную переменную
 global.app = {
     path: path,
     gulp: gulp,
+    plugins: plugins,
 }
 
 // импорт задач
 import {copy} from './gulp/tasks/copy.js';
 import {reset} from './gulp/tasks/reset.js';
+import {html} from './gulp/tasks/html.js';
 
 // Наблюдатель за изменениями в файле
 function watcher(){
-    gulp.watch(path.watch.files, copy)
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
 }
 
+const mainTasks = gulp.parallel(copy, html);
+
 //Построение сценариев выполнения задач
-const dev = gulp.series(reset, copy, watcher) // последовательность важна 
+const dev = gulp.series(reset, mainTasks, watcher) // последовательность важна 
 
 //выполнение задачи по умолчанию
 gulp.task('default', dev);
