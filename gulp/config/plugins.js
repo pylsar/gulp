61eb4e74a92c3a741dd1f146e5e75626be556778
Alex Kusov <kysov2006@bk.ru>
2021-12-26 09:31:33 +0300

    browser sync doesnt work

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,10 +1,12 @@
 import replace from 'gulp-replace'; // поиск и замена
 import plumber from 'gulp-plumber'; // обработка ошибок
 import notify from 'gulp-notify'; // подсказки
+import browserSync from 'browser-sync'; // live server
  
 // экспортируем объекты
 export const plugins = {
     replace: replace,
     plumber: plumber,
-    notify: notify
+    notify: notify,
+    browserSync: browserSync
 }
--- gulpFile.js
+++ gulpFile.js
@@ -15,20 +15,23 @@ global.app = {
 }
 
 // импорт задач
-import {copy} from './gulp/tasks/copy.js';
-import {reset} from './gulp/tasks/reset.js';
-import {html} from './gulp/tasks/html.js';
+import { copy } from './gulp/tasks/copy.js';
+import { reset } from './gulp/tasks/reset.js';
+import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
 
 // Наблюдатель за изменениями в файле
 function watcher(){
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
-}
+};
+
+
 
 const mainTasks = gulp.parallel(copy, html);
 
 //Построение сценариев выполнения задач
-const dev = gulp.series(reset, mainTasks, watcher) // последовательность важна 
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server)) // последовательность важна 
 
 //выполнение задачи по умолчанию
 gulp.task('default', dev);
